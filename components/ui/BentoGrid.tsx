d54f912fdcf3299139186dac3da7fdb6d8a364ec
zahar2324 <frontend2024zahar@gmail.com>
2025-06-16 12:24:30 +0300

    fix 2

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
+import dynamic from "next/dynamic";
 
-import { BackgroundGradientAnimation } from "./GradientBg";
+const BackgroundGradientAnimation = dynamic(
+  () => import("./GradientBg").then((mod) => mod.BackgroundGradientAnimation),
+  { ssr: false }
+);
+
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
